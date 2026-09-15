c47a4ae78f92801f685a383e4fe27c4364a8625e
Starman(StarmanMine142) <boguslavrudnev54@gmail.com>
2026-09-15 10:42:43 +0300

    pre 3, better placement

--- fabric/src/main/java/starman/aspengrove/classes/AspenGroveBiomes.java
+++ fabric/src/main/java/starman/aspengrove/classes/AspenGroveBiomes.java
@@ -1,20 +1,27 @@
 package starman.aspengrove.classes;
-//
-//import com.terraformersmc.biolith.api.biome.BiomePlacement;
 
+import com.terraformersmc.biolith.api.biome.BiomePlacement;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.*;
 import net.minecraft.world.level.biome.*;
-//
+
 public class AspenGroveBiomes {
     public static final ResourceKey<Biome> ASPEN_GROVE = ResourceKey.create(
             Registries.BIOME,
             Identifier.fromNamespaceAndPath("aspengrove", "aspen_grove")
     );
-//
+
     public static void initialize() {
-//        BiomePlacement.replaceOverworld(Biomes.FOREST, ASPEN_GROVE, 0.27);
-//        BiomePlacement.replaceOverworld(Biomes.BIRCH_FOREST, ASPEN_GROVE, 0.27);
-//        BiomePlacement.replaceOverworld(Biomes.TAIGA, ASPEN_GROVE, 0.27);
+        Climate.ParameterPoint aspenParameters = new Climate.ParameterPoint(
+                Climate.Parameter.span(-0.05F, 0.15F),
+                Climate.Parameter.span(-0.1F, 0.2F),
+                Climate.Parameter.span(0.3F, 0.7F),
+                Climate.Parameter.span(-0.375F, 0.05F),
+                Climate.Parameter.span(0.0F, 0.0F),
+                Climate.Parameter.span(0.0F, 0.3F),
+                0L
+        );
+
+        BiomePlacement.addOverworld(ASPEN_GROVE, aspenParameters);
     }
 }
